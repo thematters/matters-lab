6485e74fa734508bce9074faadee2054a9f9e8fe
Elk Leung <elkleung@gmail.com>
2022-06-12 03:50:18 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 var index = function () {
+	var slider, slide;
 	var menuDisplay = (boolean)=>{
 		let $y, $menu, $close
 		if(boolean){
@@ -49,37 +50,54 @@ var index = function () {
 		gsap.to('.orbit-4 .planets', {duration: 120, repeat: -1, rotation: 360, ease: "none"})
 	}
 	var setSlider = () => {
-		const tl = gsap.timeline({
+		slider = gsap.timeline({
 			repeat: -1,
+			repeatDelay: 5
 		})
 		gsap.to('.orbit-3 .planets', {duration: 180, repeat: -1, rotation: 360, ease: "none"})
 		gsap.to('.orbit-4 .planets', {duration: 120, repeat: -1, rotation: 360, ease: "none"})
+		gsap.set('.who_we_empower .slide', {autoAlpha: 0})
 
-		gsap.set('.who_we_empower .developers, .who_we_empower .investors', {autoAlpha: 0})
-
-		tl.to('.who_we_empower .creators', {duration: 1, autoAlpha: 1}, 0)
-			.set('.orbit-5 .planet-1', {className:"-=planet planet-1 active"}, 0)
-		
-			.to('.who_we_empower .creators', {duration: .5, autoAlpha: 0}, 3)
-		  .to('.orbit-5 .planets', {duration: 1, rotation: 111}, 3)
-		  .set('.orbit-5 .planet-1', {className:"-=planet planet-1"}, 3)
-		  
-			.to('.who_we_empower .developers', {duration: 1, autoAlpha: 1}, 3.5)
-		  .set('.orbit-5 .planet-2', {className:"-=planet planet-2 active"}, 3.5)
-		  
-			.to('.who_we_empower .developers', {duration: .5, autoAlpha: 0}, 6.5)
-		  .to('.orbit-5 .planets', {duration: 1, rotation: 250}, 6.5)
-		  .set('.orbit-5 .planet-2', {className:"-=planet planet-2"}, 6.5)
-		  
-			.to('.who_we_empower .investors', {duration: 1, autoAlpha: 1}, 7)
-		  .set('.orbit-5 .planet-3', {className:"-=planet planet-3 active"}, 7)
-
-			.to('.who_we_empower .investors', {duration: .5, autoAlpha: 0}, 10)
-		  .to('.orbit-5 .planets', {duration: 1, rotation: 360}, 10)
-		  .set('.orbit-5 .planet-3', {className:"-=planet planet-3"}, 10)
-
-			.to('.who_we_empower .creators', {duration: 1, autoAlpha: 1}, 10.5)
-			.set('.orbit-5 .planet-1', {className:"-=planet planet-1 active"}, 10.5)
+		slider.add(()=>{ setSlide('1') }, 0)
+					.add(()=>{ setSlide('2') }, 5)
+					.add(()=>{ setSlide('3') }, 10)
+	}
+	var setSlide = (val) => {
+		let $rotation
+		switch (val) {
+			case '1':
+				$rotation = 0
+				break;
+			case '2':
+				$rotation = 111
+				break;
+			case '3':
+				$rotation = 250
+				break;
+		}
+		slide = gsap.timeline({
+			paused: true
+		})
+		slide.add(()=>{
+				const $planet = document.querySelectorAll('.planet');
+				const $nav = document.querySelectorAll('.nav');
+				for (const item of $planet) {
+					item.classList.remove('active');
+				}
+				for (const item of $nav) {
+					item.classList.remove('active');
+				}
+			})
+			.add(()=>{
+				document.querySelector('.nav-'+val).classList.add("active")
+			})
+		  .to('.who_we_empower .slide', {duration: .5, autoAlpha: 0})
+			.to('.orbit-5 .planets', {duration: 1, rotation: $rotation})
+			.add(()=>{
+				document.querySelector('.planet-'+val).classList.add("active")
+			}, 1.5)
+			.to('.who_we_empower .slide-'+val, {duration: .5, autoAlpha: 1}, 1.5)
+		slide.play()
 	}
 	var setVideo = () => {
 		if(window.innerWidth >= 768){
@@ -108,6 +126,11 @@ var index = function () {
 		},
 		goto: () => {
 			toFollowUs()
+		},
+		slide: (val) => {
+			slider.pause()
+			slide.pause()
+			setSlide(val)
 		}
 	};
 }();
